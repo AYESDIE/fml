3393b639438a0ab0ca76f929ba0a2e540c923741
ayesdie <ayushchamoli8@gmail.com>
2019-06-04 20:01:40 +0530

    some magic and it probably worked fine

--- fml/methods/linear_regression/linear_regression_function.hpp
+++ fml/methods/linear_regression/linear_regression_function.hpp
@@ -8,11 +8,13 @@ namespace fml {
 namespace regression {
 
 class LinearRegressionFunction {
+public:
   LinearRegressionFunction()
   { /* This does nothing */ }
 
   LinearRegressionFunction(const arma::mat& dataset,
-                           const arma::vec& labels);
+                           const arma::vec& labels,
+                           const bool& fitIntercept);
 
   double Evaluate(const arma::mat& parameters);
 
@@ -23,28 +25,53 @@ private:
   arma::mat dataset;
 
   arma::vec labels;
+
+  bool fitIntercept;
 };
 
 LinearRegressionFunction::LinearRegressionFunction(const arma::mat &dataset,
-                                                   const arma::vec &labels) :
+                                                   const arma::vec &labels,
+                                                   const bool& fitIntercept) :
     dataset(dataset),
-    labels(labels)
+    labels(labels),
+    fitIntercept(fitIntercept)
 { /* Nothing to do here */ }
 
 double LinearRegressionFunction::Evaluate(const arma::mat &parameters) {
   arma::mat score;
 
-  score = parameters * dataset;
-  std::cout << parameters;
+  if (!fitIntercept)
+  {
+    score = parameters * dataset;
+  }
+  else
+  {
+    score = parameters.head_cols(dataset.n_rows) * dataset +
+        arma::accu(parameters.col(dataset.n_rows));
+  }
 
-  return 0;
+
+  score = score.t() - labels;
+  score %= score;
+
+  return arma::accu(score);
 }
 
 double LinearRegressionFunction::Evaluate(const arma::mat &parameters, const size_t id) {
   // Loss is evaluated as
   // Σ (h(x) - y)
   double score;
-  score = arma::accu(parameters * dataset.cols(id, 1));
+
+  if (!fitIntercept)
+  {
+    score = arma::accu(parameters * dataset.col(id));
+  }
+  else
+  {
+    score = arma::accu(parameters.head_cols(dataset.n_rows) * dataset.col(id) +
+        parameters.col(dataset.n_rows));
+  }
+
   return std::pow(score - labels(id), 2);
 }
 
--- fml/tests/linear_regression_test.cpp
+++ fml/tests/linear_regression_test.cpp
@@ -6,7 +6,7 @@
 using namespace fml;
 using namespace fml::regression;
 
-TEST_CASE("SimpleLinearRegressionFunctionTest","[LinearRegressionFunctionTest]")
+TEST_CASE("LinearRegressionFunctionTest","[LinearRegressionFunctionTest]")
 {
 
   arma::mat dataset;
@@ -16,7 +16,54 @@ TEST_CASE("SimpleLinearRegressionFunctionTest","[LinearRegressionFunctionTest]")
     return;
   }
 
-  std::cout << dataset;
+  arma::vec labels = dataset.col(2);
 
-  REQUIRE(true);
+  dataset = dataset.cols(0, 1).t();
+
+  LinearRegressionFunction lrf(dataset, labels, false);
+
+  arma::mat parameters = "1 1";
+  lrf.Evaluate(parameters);
+
+  double score = 0;
+  for (int i = 0; i < dataset.n_cols; ++i) {
+    score += lrf.Evaluate(parameters, i);
+  }
+
+  REQUIRE(score == lrf.Evaluate(parameters));
+
+  parameters = "12 -32";
+  lrf.Evaluate(parameters);
+
+  score = 0;
+  for (int i = 0; i < dataset.n_cols; ++i) {
+    score += lrf.Evaluate(parameters, i);
+  }
+
+  REQUIRE(score == lrf.Evaluate(parameters));
+}
+
+TEST_CASE("LinearRegressionFunctionTestWithIntercept","[LinearRegressionFunctionTest]") {
+
+  arma::mat dataset;
+  if (!dataset.load("data/linreg.csv", arma::csv_ascii)) {
+    FAIL("couldn't load data");
+    return;
+  }
+
+  arma::vec labels = dataset.col(2);
+
+  dataset = dataset.cols(0, 1).t();
+
+  LinearRegressionFunction lrf(dataset, labels, true);
+
+  arma::mat parameters = "1 1 1";
+  lrf.Evaluate(parameters);
+
+  double score = 0;
+  for (int i = 0; i < dataset.n_cols; ++i) {
+    score += lrf.Evaluate(parameters, i);
+  }
+
+  REQUIRE(score == lrf.Evaluate(parameters));
 }
