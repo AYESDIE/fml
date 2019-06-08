496132c437bd8deca7836aecdb5d07b1c2acbdff
ayesdie <ayushchamoli8@gmail.com>
2019-06-08 16:30:18 +0530

    making a better test case

--- fml/tests/linear_regression_test.cpp
+++ fml/tests/linear_regression_test.cpp
@@ -19,17 +19,25 @@ TEST_CASE("LinearRegression","[LinearRegression]")
   std::ifstream in_file;
   in_file.open("data/in.csv");
   auto dataset = xt::load_csv<double>(in_file);
+  in_file.close();
 
-  auto labels = xt::view(dataset, xt::all(), xt::keep(3));
-  labels = xt::linalg::norm(labels, 1);
-  std::cout << labels;
-  auto data = xt::view(dataset, xt::all(), xt::keep(0, 1, 2));
-  data = xt::linalg::norm(data, 2);
-  std::cout << data;
+  in_file.open("data/res.csv");
+  auto result = xt::load_csv<double>(in_file);
+
+  auto labels = xt::view(dataset, xt::all(), xt::keep(2));
+  auto data = xt::view(dataset, xt::all(), xt::keep(0, 1));
 
   fml::regression::LinearRegression lr(data, labels);
 
-  REQUIRE(true);
+  xt::xarray<double> predictions;
+  lr.Compute(data, predictions);
+
+  auto resultiter = result.begin();
+  auto prediter = predictions.begin();
+  for (; prediter != predictions.end(); prediter++, resultiter++)
+  {
+    REQUIRE(*prediter == Approx(*resultiter).margin(1e-5));
+  }
 }
 
 TEST_CASE("LinearRegression2","[LinearRegression]")
