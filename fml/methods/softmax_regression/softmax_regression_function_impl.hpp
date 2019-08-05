a0a5fd800eb266f8ec5aefd785a41b7dcdb8b3ed
Ayush Chamoli <ayushchamoli8@gmail.com>
2019-08-05 19:38:38 +0530

    updates

--- fml/methods/softmax_regression/softmax_regression_function_impl.hpp
+++ fml/methods/softmax_regression/softmax_regression_function_impl.hpp
@@ -28,16 +28,25 @@ template <typename DatasetType, typename LabelsType>
 template <typename E>
 double SoftmaxRegressionFunction<DatasetType, LabelsType>::Evaluate(const E& parameters)
 {
-  // Evaluate the loss using sigmoid function
-  auto sigmoid =  1 / (1 + xt::exp(-xt::linalg::dot(dataset, parameters)));
-  auto error = - (labels * xt::log(sigmoid)) - ((1 - labels) * xt::log(1 - sigmoid));
-  double loss = xt::sum(error / numFunctions())();
+//  // Evaluate the loss using sigmoid function
+//  auto sigmoid =  1 / (1 + xt::exp(-xt::linalg::dot(dataset, parameters)));
+//  auto error = - (labels * xt::log(sigmoid)) - ((1 - labels) * xt::log(1 - sigmoid));
+//  double loss = xt::sum(error / numFunctions())();
+//
+//  // Evaluate the regularization
+//  double reg = (lambda / (2 * numFunctions()))
+//      * xt::linalg::dot(xt::transpose(parameters), parameters)();
+//
+//  return loss + reg;
+  std::cout << xt::linalg::dot(dataset, parameters);
 
-  // Evaluate the regularization
-  double reg = (lambda / (2 * numFunctions()))
-      * xt::linalg::dot(xt::transpose(parameters), parameters)();
+  auto scores = xt::linalg::dot(dataset, parameters);
+  auto exp_scores = xt::exp(scores);
 
-  return loss + reg;
+  auto prob_scores = exp_scores / xt::sum(exp_scores, 1);
+  // auto prob_scores = 1;
+  std::cout << prob_scores;
+  return 0;
 }
 
 template<typename DatasetType, typename LabelsType>
@@ -49,7 +58,7 @@ size_t SoftmaxRegressionFunction<DatasetType, LabelsType>::numFunctions()
 template<typename DatasetType, typename LabelsType>
 xt::xtensor<double, 2> SoftmaxRegressionFunction<DatasetType, LabelsType>::GetInitialPoints()
 {
-  return xt::zeros<xt::xarray<double>>({int(dataset.shape(1)), numClasses});
+  return xt::ones<xt::xarray<double>>({size_t(dataset.shape(1)), numClasses});
 }
 
 } // namespace regression
